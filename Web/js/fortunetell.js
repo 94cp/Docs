52a36794ee61a94a0a8316a9bd8639419518fa53
cp110 <1107223894@qq.com>
2020-12-10 00:56:00 +0800

    update

--- Web/js/fortunetell.js
+++ Web/js/fortunetell.js
@@ -4,33 +4,50 @@ const days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 ];
 
 function parseString(params) {
-    
+
 }
 
 function getSteps(datas) {
     return datas.length.toString(2).length;
 }
 
-function gets(datas) {
-    const steps = getSteps(datas);
+function getStepArrays(datas, steps) {
     var arrs = Array();
     for (let i = 0; i < steps; i++) {
         arrs.push(Array());
     }
     for (let i = 0; i < datas.length; i++) {
         const binaryIndex = (i + 1).toString(2);
-        for (let j = binaryIndex.length - 1; j >= 0; j--) {
+        const lastIndex = binaryIndex.length - 1;
+        for (let j = lastIndex; j >= 0; j--) {
             const temp = binaryIndex[j];
             if (temp == 1) {
-                arrs[j].push(datas[i]);
+                arrs[lastIndex - j].push(datas[i]);
             }
         }
     }
     return arrs;
 }
 
-function myFunction(){
-	var x = document.getElementById("demo");
-	x.innerHTML= gets(days);
+function myFunction() {
+    const steps = getSteps(days);
+    const stepArrs = getStepArrays(days, steps);
+
+
+    for (let step = 0; step < steps; step++) {
+        const length = steps - 1;
+        const table = document.createElement("table");
+        for (let trIndex = 0; trIndex < length; trIndex++) {
+            const tr = document.createElement("tr");
+            for (let tdIndex = 0; tdIndex < length; tdIndex++) {
+                const td = document.createElement("td");
+                td.innerHTML = stepArrs[step][trIndex * length + tdIndex];
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        }
+
+        let fortunetellDiv = document.getElementById("fortunetellDiv");
+        fortunetellDiv.appendChild(table);
+    }
 }
-// https://www.cnblogs.com/leftshine/p/GuessBirthday.html
